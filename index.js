8a0a68d1e3173bcf1ad03a06c611d52764506b18
setare <sayyadsetareh64@gmail.com>
2025-04-05 04:24:43 +0330

    completed

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("applyBtn").addEventListener("click", function () {
+  const formSection = document.getElementById("formSection");
+  const landingSection = document.getElementById("landing");
+
+  landingSection.style.display = "none";
+  formSection.style.display = "block";
+
+  formSection.scrollIntoView({ behavior: "smooth" });
+});
